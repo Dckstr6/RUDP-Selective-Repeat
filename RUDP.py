9d238309128c632666d9ab2365f311e622f801dc
Pranay Tarigopula <pranaytarigopula@Pranays-MBP.local>
2021-03-19 15:21:10 -0400

    Modified Connection Class

--- RUDP.py
+++ RUDP.py
@@ -6,12 +6,38 @@ class Connection:
 	packet_size = 0
 	packet = 0
 	s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+	target_host = 0
+	timeoutval = 0
+	fragments = list()
+	port = 0
 
-	def __init__(self,buffer_size,window_size,packet_size,packet):
+	def __init__(self,buffer_size,window_size,packet_size,packet,target_host,port,timeoutval):
 		self.buffer_size = buffer_size
 		self.window_size = window_size
 		self.packet_size = packet_size
 		self.packet = packet
+		self.target_host = target_host
+		self.port = port
+		self.timeoutval = timeoutval
+
+	def connect(self):
+		self.s.connect((str(self.target_host),int(self.port)))
+
+	def send(self):
+		msglen = len(self.packet)
+		self.s.sendto(self.packet,(self.target_host,self.port))
+
+	def recv(self):
+		while True:
+			chunk,addr = self.s.recvfrom(65535)
+			if not chunk:
+				break
+			fragments.append(chunk)
+
+	def settimeout(self):
+
+
+
 
 class Packet:
 	payload = ""
